711dcc2aecbb6d755a1f8d3c41b1d1105fc3e356
Aleksei Ovcharov <senserlex@gmail.com>
2018-06-05 17:25:29 +0300

    Added first angle calculation.

--- src/LTP.cpp
+++ src/LTP.cpp
@@ -1,5 +1,9 @@
 #include <local_task_planner/LTP.h>
 
+#include <tf2_ros/transform_listener.h>
+
+#include <geometry_msgs/TransformStamped.h>
+
 
 localTP::localTP(ros::NodeHandle & n, Configuration conf)
 {
@@ -21,18 +25,19 @@ bool localTP::localTaskCallback(std_srvs::Empty::Request  & req,
 {
     ROS_INFO("[LTP] Start task execution");
 
-    red_msgs::Pose pose;
+    red_msgs::Pose recognPose;
     red_msgs::ArmPoses manipPoses;
 
     // Set initial position of manipulator for object recognition
-    pose.x = 0.2; pose.y = 0; pose.z = 0.1;
-    pose.theta = 3.1415; pose.psi = 0;
+    recognPose.x = 0.2; recognPose.y = 0; recognPose.z = 0.1;
+    recognPose.theta = 3.1415; recognPose.psi = 0;
     ROS_INFO("[LTP] Go to first position.");
-    ROS_INFO("[LTP] Pose: [%f, %f, %f, %f, %f]", pose.x, pose.y, pose.z, pose.theta, pose.psi);
-    manipPoses.request.poses.push_back(pose);
+    ROS_INFO("[LTP] recognPose: [%f, %f, %f, %f, %f]", recognPose.x, recognPose.y, recognPose.z, recognPose.theta, recognPose.psi);
+    manipPoses.request.poses.push_back(recognPose);
     if (manipulationPointClient.call(manipPoses)) {
         std::cout << "\t Successfull." << std::endl;
     }
+    manipPoses.request.poses.clear();
 
     // Communicate with camera
     ROS_INFO("[LTP] Set request to camera.");
@@ -43,6 +48,7 @@ bool localTP::localTaskCallback(std_srvs::Empty::Request  & req,
         int err = cameraTask.response.error;
         std::cout << "Error: " << err << std::endl;
         for (size_t i = 0; i < cameraTask.response.poses.size(); ++i) {
+            // Transform between arm_link_2 and arm_link_5
             std::cout << "Pose: " << i << std::endl;
             std::cout << "\t x:     \t" << cameraTask.response.poses[i].x << std::endl;
             std::cout << "\t y:     \t" << cameraTask.response.poses[i].y << std::endl;
@@ -55,6 +61,31 @@ bool localTP::localTaskCallback(std_srvs::Empty::Request  & req,
             std::cout << "------------------------------------------" << std::endl;
         }
     }
+
+    // Activate tf for search transform
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener(tfBuffer);
+
+    // Find manipulator configuration for optical axis of camera
+    JointValues optq; red_msgs::Pose objectTransform;
+    objectTransform.x = cameraTask.response.poses[0].x;
+    objectTransform.y = cameraTask.response.poses[0].y;
+    objectTransform.z = cameraTask.response.poses[0].z;
+    optq(0) = atan2(recognPose.y, recognPose.x);
+
+    geometry_msgs::TransformStamped transformStamped;
+    try{
+        transformStamped = tfBuffer.lookupTransform("realsense_camera", "arm_link_2", ros::Time(1));
+    } catch (tf2::TransformException &ex) {
+        ROS_WARN("%s",ex.what());
+        ros::Duration(1.0).sleep();
+    }
+
+    optq(0) += atan2(objectTransform.y, objectTransform.x)
+        - atan2(transformStamped.transform.translation.y, transformStamped.transform.translation.x);
+
+    ROS_INFO("Angle q1: %f", optq(0));
+
 
     return true;
 }
